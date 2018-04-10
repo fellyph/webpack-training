7c5e5b39a1750ac8e5ff3a57aa8ddd2b17cb5d21
Cintra <fcintra@deloitte.ie>
2018-04-10 17:07:10 +0100

    adding first files

---
+++ src/form.js
@@ -0,0 +1,25 @@
+export class FormLogin {
+  constructor (user, password) {
+    this.user = user
+    this.password = password
+    this.logged = false
+  }
+
+  login (user, password) {
+    if (user !== this.user) {
+      return 'invalid user'
+    }
+    if (password === this.password) {
+      this.logged = true
+      return 'user logged'
+    }
+  }
+
+  showCurrentUser () {
+    if (this.logged) {
+      return this.user
+    } else {
+      return 'unlogged'
+    }
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './less/main.less'
+import FormLogin from './form'
+
+const form = new FormLogin('Fellyph', 'test123')
+
+form.showCurrentUser()
